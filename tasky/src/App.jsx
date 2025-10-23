729975de2f31cf1e1d9b7dfadee24b562afa3060
Leon-Ogunfeyimi <20108255@mail.wit.ie>
2025-10-23 22:48:58 +0100

    Added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,19 +5,21 @@ import React, { useState } from 'react';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
+  
     return (
     <div className="container">
       <h1>Tasky</h1>
         {taskState.tasks.map((task) => (
           <Task 
           title={task.title}
-          description={task.description} //descriptions wasn't coming up so I had to update my Task.jsx to view them
+          description={task.description}
           deadline={task.deadline}
+          key={task.id}
           />
           ))} 
         </div>
